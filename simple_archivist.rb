23778637dc3dea796cf0784a59a2a28fd4568cc0
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2014-12-06 21:55:38 +0000

    Created very simple DirectoryPair class.

--- simple_archivist.rb
+++ simple_archivist.rb
@@ -1,2 +1,12 @@
 puts "Simple Archivist v. 0.1.0"
 
+class DirectoryPair
+  def initialize(left_dir, right_dir)
+    puts "Initialising root directory pair..."
+    puts "Left directory: #{left_dir}"
+    puts "Right directory: #{right_dir}"
+  end
+end
+
+DirectoryPair.new("/tmp", "/home/Mike/Development/Temp")
+
